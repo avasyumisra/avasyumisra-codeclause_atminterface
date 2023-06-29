190a195378513e75ab2a8eb1ae67c2b1b1bb0cd4
avasyumisra <131693250+avasyumisra@users.noreply.github.com>
2023-06-29 17:16:47 +0530

    atm.java

---
+++ atm.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+public class ATM extends OptionMenu{
+
+  public static void main(String[] args) throws IOException {
+    OptionMenu optionMenu= new OptionMenu();
+public class ATM extends OptionMenu {
+
+    optionMenu.getLogin();
+  }
+	public static void main(String[] args) throws IOException {
+		OptionMenu optionMenu = new OptionMenu();
+		introduction();
+		optionMenu.mainMenu();
+	}
+
+};
+	public static void introduction() {
+		System.out.println("Welcome to the ATM Project!");
+	}
+}
